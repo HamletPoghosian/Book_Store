1efdfb58041ba42972a79012097a58eea055c986
HamletPoghosian <phamik@mail.ru>
2019-05-17 11:22:52 +0400

    add new BookView Class and BookControler add new Views and new Methods

---
+++ Book_Store/Book_Store/ViewModels/BookView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Book_Store.ViewModels
+{
+    public class BookView
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public double Price { get; set; }
+        public double Popular { get; set; }
+    }
+}
