afbb9da67cc4533ba6acddab0b862dcbac76e6e5
HamletPoghosian <phamik@mail.ru>
2019-05-17 11:06:03 +0400

    Add DB context model and creating DB for entity

--- Book_Store/Book_Store/Models/BookDBContext.cs
+++ Book_Store/Book_Store/Models/BookDBContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,5 +9,26 @@ namespace Book_Store.Models
 {
     public class BookDBContext:DbContext
     {
+        public BookDBContext(DbContextOptions<BookDBContext> options):base(options)
+        {
+        }
+        public DbSet<Book> BookItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>(e =>
+            {
+                e.HasKey(b => b.Id);
+                e.Property(b => b.Id).HasDefaultValueSql("(newid())");
+                e.Property(b => b.Name).IsRequired();
+                e.Property(b => b.Author).IsRequired();
+                e.Property(b => b.Price).IsRequired();
+                e.Property(b => b.Popular).IsRequired();
+
+            });
+
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
