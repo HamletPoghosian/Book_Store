12bfc9fd403125c9826f00a31ade37ae5e2f8e80
HamletPoghosian <phamik@mail.ru>
2019-05-17 10:55:27 +0400

    add new class Book and new DB context

--- Book_Store/Book_Store/Startup.cs
+++ Book_Store/Book_Store/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Book_Store.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -31,7 +32,7 @@ namespace Book_Store
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
