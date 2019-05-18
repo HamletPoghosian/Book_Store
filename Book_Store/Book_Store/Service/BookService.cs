87405b21afdf8d95c127e6ecdb7ed1da3b927004
HamletPoghosian <phamik@mail.ru>
2019-05-18 15:30:48 +0400

    Add new function for BookService

--- Book_Store/Book_Store/Service/BookService.cs
+++ Book_Store/Book_Store/Service/BookService.cs
@@ -1,11 +1,52 @@
-﻿using System;
+﻿using Book_Store.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Book_Store.Service
 {
-    public class BookService
+    public class BookService : IBookService
     {
+        BookDBContext _context;
+        public BookService(BookDBContext context)
+        {
+            _context = context;
+        }
+        public async Task<BookItems> AddItemsAsync(BookItems book)
+        {
+            var entity = new Book
+            {
+                Id = book.Id,
+                Author = book.Author,
+                Name = book.Name,
+                Popular = book.Popular,
+                Price = book.Price
+            };
+            await _context.BookItems.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            book.Id = entity.Id;
+            return book;
+        }
+
+        public async Task DeleteAsync(BookItems bookItems)
+        {
+            var entity = await _context.BookItems.SingleOrDefaultAsync(e => e.Id == bookItems.Id);
+            _context.BookItems.Remove(entity);
+            await _context.SaveChangesAsync();
+
+
+        }
+
+        public Task EditAsync(BookItems book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BookItems> GetBookAsync(BookItems book)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
