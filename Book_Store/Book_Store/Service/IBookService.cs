1cc8358e0b8788e9485945437bfd1707e1307e49
HamletPoghosian <phamik@mail.ru>
2019-05-18 15:15:26 +0400

    add new Interface

--- Book_Store/Book_Store/Service/IBookService.cs
+++ Book_Store/Book_Store/Service/IBookService.cs
@@ -7,6 +7,9 @@ namespace Book_Store.Service
 {
     public interface IBookService
     {
-        
+        Task<BookItems> AddItemsAsync(BookItems book);
+        Task EditAsync(BookItems book);
+        Task DeleteAsync(BookItems bookItems);
+        Task<BookItems> GetBookAsync(BookItems book);
     }
 }
