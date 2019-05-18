dd333825b7d4ec97ad9e3524eab6e96bfb24d483
HamletPoghosian <phamik@mail.ru>
2019-05-18 16:04:46 +0400

    add new extension method

---
+++ Book_Store/Book_Store/Service/BookServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Book_Store.Service
+{
+    public static class BookServiceExtensions
+    {
+        public static IServiceCollection AddBook(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection.AddScoped<IBookService, BookService>();
+        }
+    }
+    }
