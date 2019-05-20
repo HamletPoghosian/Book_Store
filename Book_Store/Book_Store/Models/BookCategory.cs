1f340d93603b7994e4474f0e276a61dc76168bb9
HamletPoghosian <phamik@mail.ru>
2019-05-20 14:26:15 +0400

    Add new Category class

---
+++ Book_Store/Book_Store/Models/BookCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Book_Store.Models
+{
+    public class BookCategory
+    {
+        public Guid Id { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+    }
+}
